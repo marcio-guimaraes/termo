712908e5f2d3d4e4b62be76358937ad7179f1817
marcio-guimaraes <mv601200@gmail.com>
2025-06-13 17:23:20 -0300

    ✨feat: adicionando tramento para acentos

--- termo.c
+++ termo.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <glib.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,10 +9,8 @@
 #define MAX_TENTATIVAS 6
 #define TAMANHO_PALAVRA 5
 #define MAX_WORD_LENGTH 100
-// IMPORTANTE: Ajuste estes valores conforme o tamanho dos seus dicionários
-#define MAX_WORDS_SORTEIO 1000      // Para o "dicionario.txt"
-#define MAX_WORDS_VALIDACAO 30000   // Para o "palavras_existentes.txt"
-
+#define MAX_WORDS_SORTEIO 1000
+#define MAX_WORDS_VALIDACAO 30000
 
 // --- Variáveis Globais ---
 char palavraCerta[MAX_WORD_LENGTH];
@@ -19,59 +18,65 @@ GtkWidget *grid;
 GtkWidget *tecladoGrid;
 GtkWidget *teclas[26];
 GtkWidget *labelTentativas;
-GtkWidget *labelMensagem; 
-
-// Array 2D para guardar os labels da grelha
+GtkWidget *labelMensagem;
 GtkWidget *grid_tiles[MAX_TENTATIVAS][TAMANHO_PALAVRA];
-
 int mapeamentoTeclas[26];
 int tentativaAtual = 0;
-
-// Dicionário para sortear a palavra secreta
 char palavras_sorteio[MAX_WORDS_SORTEIO][MAX_WORD_LENGTH];
 int numPalavras_sorteio;
-
-// Dicionário com todas as palavras válidas para validação
 char palavras_existentes[MAX_WORDS_VALIDACAO][MAX_WORD_LENGTH];
 int numPalavras_existentes;
-// --- Fim das Variáveis Globais ---
 
-
-// Declaração de funções
+// --- Protótipos de Funções ---
 void mostrarTelaVitoria(GtkWidget *parent);
+void iniciar_jogo_termo(int argc, char *argv[]);
+
+// --- Funções Auxiliares ---
 
 gboolean limpar_label_mensagem(gpointer data) {
     gtk_label_set_text(GTK_LABEL(data), "");
     return G_SOURCE_REMOVE;
 }
 
-gboolean palavra_existe_no_dicionario(const char *palavra) {
-    for (int i = 0; i < numPalavras_existentes; i++) {
-        if (strcmp(palavra, palavras_existentes[i]) == 0) {
-            return TRUE;
+void normalizar_palavra(const char *input, char *output) {
+    gchar *normalized_string = g_utf8_normalize(input, -1, G_NORMALIZE_NFD);
+    gchar *out_ptr = output;
+    const gchar *p = normalized_string;
+    while (*p) {
+        gunichar uc = g_utf8_get_char(p);
+        if (!g_unichar_ismark(uc)) {
+            gunichar upper_char = g_unichar_toupper(uc);
+            out_ptr += g_unichar_to_utf8(upper_char, out_ptr);
         }
+        p = g_utf8_next_char(p);
     }
-    return FALSE;
+    *out_ptr = '\0';
+    g_free(normalized_string);
 }
 
-void carregarPalavras(const char *nomeArquivo, char palavras_array[][MAX_WORD_LENGTH], int *numPalavras_ptr)
-{
+const char* validar_e_corrigir_palavra(const char *tentativa_normalizada) {
+    char palavra_dicionario_normalizada[MAX_WORD_LENGTH];
+    for (int i = 0; i < numPalavras_existentes; i++) {
+        normalizar_palavra(palavras_existentes[i], palavra_dicionario_normalizada);
+        if (strcmp(tentativa_normalizada, palavra_dicionario_normalizada) == 0) {
+            return palavras_existentes[i];
+        }
+    }
+    return NULL;
+}
+
+void carregarPalavras(const char *nomeArquivo, char palavras_array[][MAX_WORD_LENGTH], int *numPalavras_ptr) {
     FILE *arquivo = fopen(nomeArquivo, "r");
-    if (!arquivo)
-    {
+    if (!arquivo) {
         fprintf(stderr, "Erro ao abrir o arquivo: %s\n", nomeArquivo);
         exit(1);
     }
-
     *numPalavras_ptr = 0;
     int max_words = (palavras_array == palavras_sorteio) ? MAX_WORDS_SORTEIO : MAX_WORDS_VALIDACAO;
-    
     char buffer[MAX_WORD_LENGTH];
-    while (*numPalavras_ptr < max_words && fgets(buffer, MAX_WORD_LENGTH, arquivo))
-    {
-        buffer[strcspn(buffer, "\r\n")] = '\0'; 
-        if (strlen(buffer) == TAMANHO_PALAVRA)
-        {
+    while (*numPalavras_ptr < max_words && fgets(buffer, MAX_WORD_LENGTH, arquivo)) {
+        buffer[strcspn(buffer, "\r\n")] = '\0';
+        if (g_utf8_strlen(buffer, -1) == TAMANHO_PALAVRA) {
             strcpy(palavras_array[*numPalavras_ptr], buffer);
             (*numPalavras_ptr)++;
         }
@@ -79,202 +84,168 @@ void carregarPalavras(const char *nomeArquivo, char palavras_array[][MAX_WORD_LE
     fclose(arquivo);
 }
 
-void selecionarPalavraAleatoria(char palavras_param[][MAX_WORD_LENGTH], int numPalavras_param, char *palavraSelecionada)
-{
+void selecionarPalavraAleatoria(char palavras_param[][MAX_WORD_LENGTH], int numPalavras_param, char *palavraSelecionada) {
     srand(time(NULL));
     int index = rand() % numPalavras_param;
     strcpy(palavraSelecionada, palavras_param[index]);
 }
 
-void aplicarCor(GtkWidget *widget, const char *corCSS)
-{
+void aplicarCor(GtkWidget *widget, const char *corCSS) {
     GtkStyleContext *context = gtk_widget_get_style_context(widget);
     GtkCssProvider *provider = gtk_css_provider_new();
-
     char css[128];
     snprintf(css, sizeof(css), "* { background-color: %s; color: white; font-weight: bold; }", corCSS);
-
     gtk_css_provider_load_from_data(provider, css, -1, NULL);
     gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     g_object_unref(provider);
 }
 
-void inicializarTeclado(GtkWidget *caixaVertical)
-{
+void inicializarTeclado(GtkWidget *caixaVertical) {
     tecladoGrid = gtk_grid_new();
     gtk_grid_set_row_spacing(GTK_GRID(tecladoGrid), 5);
     gtk_grid_set_column_spacing(GTK_GRID(tecladoGrid), 5);
     gtk_box_pack_end(GTK_BOX(caixaVertical), tecladoGrid, FALSE, FALSE, 5);
-
     const char *layout = "QWERTYUIOPASDFGHJKLZXCVBNM";
     int linha = 0, coluna = 0;
-
-    for (int i = 0; i < 26; i++)
-    {
+    for (int i = 0; i < 26; i++) {
         char letra[2] = {layout[i], '\0'};
         GtkWidget *label = gtk_label_new(letra);
         gtk_widget_set_hexpand(label, TRUE);
         gtk_widget_set_vexpand(label, TRUE);
         gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
         gtk_widget_set_valign(label, GTK_ALIGN_CENTER);
-        
         GtkStyleContext *context = gtk_widget_get_style_context(label);
         gtk_style_context_add_class(context, "tecla-teclado");
-
         gtk_grid_attach(GTK_GRID(tecladoGrid), label, coluna, linha, 1, 1);
         teclas[i] = label;
-
         mapeamentoTeclas[layout[i] - 'A'] = i;
-
         coluna++;
-        if ((i == 9) || (i == 18))
-        {
+        if ((i == 9) || (i == 18)) {
             linha++;
             coluna = 0;
         }
     }
 }
 
-void atualizarTeclado(const char *tentativa, const char *palavraCerta)
-{
+void atualizarTeclado(const char *palavra_tentada, const char *palavra_certa_real) {
+    char tentada_norm[MAX_WORD_LENGTH], certa_norm[MAX_WORD_LENGTH];
+    normalizar_palavra(palavra_tentada, tentada_norm);
+    normalizar_palavra(palavra_certa_real, certa_norm);
     int letrasUsadas[TAMANHO_PALAVRA] = {0};
 
-    for (int i = 0; i < TAMANHO_PALAVRA; i++)
-    {
-        int indice = mapeamentoTeclas[toupper(tentativa[i]) - 'A'];
-        if (tentativa[i] == palavraCerta[i])
-        {
-            aplicarCor(teclas[indice], "#32CD32"); // Verde
+    for (int i = 0; i < TAMANHO_PALAVRA; i++) {
+        if (tentada_norm[i] == certa_norm[i]) {
+            int indice = mapeamentoTeclas[tentada_norm[i] - 'A'];
+            if (indice >= 0 && indice < 26) aplicarCor(teclas[indice], "#32CD32");
             letrasUsadas[i] = 1;
         }
     }
-
-    for (int i = 0; i < TAMANHO_PALAVRA; i++)
-    {
-        int indice = mapeamentoTeclas[toupper(tentativa[i]) - 'A'];
-        if (tentativa[i] != palavraCerta[i])
-        {
+    for (int i = 0; i < TAMANHO_PALAVRA; i++) {
+        if (tentada_norm[i] != certa_norm[i]) {
             int achou = 0;
-            for (int j = 0; j < TAMANHO_PALAVRA; j++)
-            {
-                if (tentativa[i] == palavraCerta[j] && !letrasUsadas[j])
-                {
+            for (int j = 0; j < TAMANHO_PALAVRA; j++) {
+                if (tentada_norm[i] == certa_norm[j] && !letrasUsadas[j]) {
                     achou = 1;
                     letrasUsadas[j] = 1;
                     break;
                 }
             }
-            if (achou)
-            {
-                aplicarCor(teclas[indice], "#FFD700"); // Amarelo
+            if (achou) {
+                int indice = mapeamentoTeclas[tentada_norm[i] - 'A'];
+                if (indice >= 0 && indice < 26) aplicarCor(teclas[indice], "#FFD700");
             }
         }
     }
-
-    for (int i = 0; i < TAMANHO_PALAVRA; i++)
-    {
-        int indice = mapeamentoTeclas[toupper(tentativa[i]) - 'A'];
+    for (int i = 0; i < TAMANHO_PALAVRA; i++) {
         int achou = 0;
-
-        for (int j = 0; j < TAMANHO_PALAVRA; j++)
-        {
-            if (tentativa[i] == palavraCerta[j])
-            {
+        for (int j = 0; j < TAMANHO_PALAVRA; j++) {
+            if (tentada_norm[i] == certa_norm[j]) {
                 achou = 1;
                 break;
             }
         }
-
-        if (!achou)
-        {
-            aplicarCor(teclas[indice], "#696969"); // Cinza
+        if (!achou) {
+            int indice = mapeamentoTeclas[tentada_norm[i] - 'A'];
+            if (indice >= 0 && indice < 26) aplicarCor(teclas[indice], "#696969");
         }
     }
 }
 
-void on_submit_clicked(GtkButton *botao, gpointer entryPtr)
-{
-    if (tentativaAtual >= MAX_TENTATIVAS)
-        return;
+void on_submit_clicked(GtkButton *botao, gpointer entryPtr) {
+    if (tentativaAtual >= MAX_TENTATIVAS) return;
 
     const char *entrada = gtk_entry_get_text(GTK_ENTRY(entryPtr));
-    if (strlen(entrada) != TAMANHO_PALAVRA)
-        return;
+    if (g_utf8_strlen(entrada, -1) != TAMANHO_PALAVRA) return;
 
-    char tentativa[6];
-    strncpy(tentativa, entrada, TAMANHO_PALAVRA);
-    tentativa[TAMANHO_PALAVRA] = '\0';
+    char tentativa_normalizada[MAX_WORD_LENGTH];
+    normalizar_palavra(entrada, tentativa_normalizada);
 
-    for (int i = 0; tentativa[i]; i++)
-        tentativa[i] = toupper((unsigned char)tentativa[i]);
+    const char *palavra_corrigida = validar_e_corrigir_palavra(tentativa_normalizada);
 
-    if (!palavra_existe_no_dicionario(tentativa)) {
+    if (palavra_corrigida == NULL) {
         gtk_label_set_text(GTK_LABEL(labelMensagem), "Palavra inválida!");
         g_timeout_add(2000, limpar_label_mensagem, labelMensagem);
         gtk_entry_set_text(GTK_ENTRY(entryPtr), "");
         return;
     }
-    
-    atualizarTeclado(tentativa, palavraCerta);
+
+    atualizarTeclado(palavra_corrigida, palavraCerta);
 
     int usadas_palavra[TAMANHO_PALAVRA] = {0};
-
-    for (int i = 0; i < TAMANHO_PALAVRA; i++)
-    {
+    
+    const gchar *p_corr = palavra_corrigida;
+    for(int i = 0; i < TAMANHO_PALAVRA; i++) {
         GtkWidget *label = grid_tiles[tentativaAtual][i];
-        
-        char letraStr[2] = {tentativa[i], '\0'};
-        gtk_label_set_text(GTK_LABEL(label), letraStr);
-
-        if (tentativa[i] == palavraCerta[i])
-        {
-            aplicarCor(label, "#32CD32");
+        char char_str[8] = {0};
+        strncpy(char_str, p_corr, g_utf8_next_char(p_corr) - p_corr);
+        gtk_label_set_text(GTK_LABEL(label), char_str);
+        p_corr = g_utf8_next_char(p_corr);
+    }
+    
+    p_corr = palavra_corrigida;
+    const gchar *p_cert = palavraCerta;
+    for (int i = 0; i < TAMANHO_PALAVRA; i++) {
+        gunichar uc_corr = g_utf8_get_char(g_utf8_offset_to_pointer(p_corr, i));
+        gunichar uc_cert = g_utf8_get_char(g_utf8_offset_to_pointer(p_cert, i));
+        if (uc_corr == uc_cert) {
+            aplicarCor(grid_tiles[tentativaAtual][i], "#32CD32");
             usadas_palavra[i] = 1;
         }
     }
-
-    for (int i = 0; i < TAMANHO_PALAVRA; i++)
-    {
-        GtkWidget *label = grid_tiles[tentativaAtual][i];
-
-        if (tentativa[i] == palavraCerta[i])
-            continue;
-
+    for (int i = 0; i < TAMANHO_PALAVRA; i++) {
+        gunichar uc_corr = g_utf8_get_char(g_utf8_offset_to_pointer(p_corr, i));
+        gunichar uc_cert_i = g_utf8_get_char(g_utf8_offset_to_pointer(p_cert, i));
+        if (uc_corr == uc_cert_i) continue;
+        
         int achou = 0;
-        for (int j = 0; j < TAMANHO_PALAVRA; j++)
-        {
-            if (!usadas_palavra[j] && tentativa[i] == palavraCerta[j])
-            {
+        for (int j = 0; j < TAMANHO_PALAVRA; j++) {
+            gunichar uc_cert_j = g_utf8_get_char(g_utf8_offset_to_pointer(p_cert, j));
+            if (!usadas_palavra[j] && uc_corr == uc_cert_j) {
                 achou = 1;
                 usadas_palavra[j] = 1;
                 break;
             }
         }
-        aplicarCor(label, achou ? "#FFD700" : "#696969");
+        aplicarCor(grid_tiles[tentativaAtual][i], achou ? "#FFD700" : "#696969");
     }
 
     tentativaAtual++;
-
+    
+    // --- LINHA CORRIGIDA ---
     char texto_tentativas[50];
     int tentativas_restantes = MAX_TENTATIVAS - tentativaAtual;
-    sprintf(texto_tentativas, "Tentativas restantes: %d", tentativas_restantes);
+    snprintf(texto_tentativas, sizeof(texto_tentativas), "Tentativas restantes: %d", tentativas_restantes);
     gtk_label_set_text(GTK_LABEL(labelTentativas), texto_tentativas);
+    // --- FIM DA CORREÇÃO ---
 
     gtk_entry_set_text(GTK_ENTRY(entryPtr), "");
 
-    if (strcmp(tentativa, palavraCerta) == 0)
-    {
+    if (strcmp(palavra_corrigida, palavraCerta) == 0) {
         mostrarTelaVitoria(gtk_widget_get_toplevel(GTK_WIDGET(botao)));
-    }
-    else if (tentativaAtual == MAX_TENTATIVAS)
-    {
-        char mensagem[128];
-        sprintf(mensagem, "A palavra era: %s", palavraCerta);
-
-        GtkWidget *dialog = gtk_message_dialog_new(
-            GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(botao))),
-            GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK,
-            "%s", mensagem);
+    } else if (tentativaAtual == MAX_TENTATIVAS) {
+        char mensagem[256];
+        snprintf(mensagem, sizeof(mensagem), "A palavra era: %s", palavraCerta);
+        GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(botao))), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "%s", mensagem);
         gtk_window_set_title(GTK_WINDOW(dialog), "Fim de Jogo");
         gtk_dialog_run(GTK_DIALOG(dialog));
         gtk_widget_destroy(dialog);
@@ -282,24 +253,17 @@ void on_submit_clicked(GtkButton *botao, gpointer entryPtr)
     }
 }
 
-
-void mostrarTelaVitoria(GtkWidget *parent)
-{
-    GtkWidget *dialog = gtk_message_dialog_new(
-        GTK_WINDOW(parent),
-        GTK_DIALOG_MODAL,
-        GTK_MESSAGE_INFO,
-        GTK_BUTTONS_OK,
-        "Parabéns! A palavra era %s", palavraCerta);
+void mostrarTelaVitoria(GtkWidget *parent) {
+    char mensagem[256];
+    snprintf(mensagem, sizeof(mensagem), "Parabéns, você acertou!\n\nA palavra era: %s", palavraCerta);
+    GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(parent), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "%s", mensagem);
     gtk_window_set_title(GTK_WINDOW(dialog), "Vitória!");
     gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
     gtk_window_close(GTK_WINDOW(parent));
 }
 
-
-void iniciar_jogo_termo(int argc, char *argv[])
-{
+void iniciar_jogo_termo(int argc, char *argv[]) {
     tentativaAtual = 0;
     gtk_init(&argc, &argv);
 
@@ -326,8 +290,7 @@ void iniciar_jogo_termo(int argc, char *argv[])
 
     GtkCssProvider *css_provider = gtk_css_provider_new();
     gtk_css_provider_load_from_data(css_provider, css_data, -1, NULL);
-    gtk_style_context_add_provider_for_screen(
-        gdk_screen_get_default(), GTK_STYLE_PROVIDER(css_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(css_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     g_object_unref(css_provider);
 
     g_signal_connect(janela, "destroy", G_CALLBACK(gtk_main_quit), NULL);
@@ -338,7 +301,7 @@ void iniciar_jogo_termo(int argc, char *argv[])
     labelTentativas = gtk_label_new(NULL);
     gtk_widget_set_name(labelTentativas, "tentativas");
     char texto_inicial[50];
-    sprintf(texto_inicial, "Tentativas restantes: %d", MAX_TENTATIVAS);
+    snprintf(texto_inicial, sizeof(texto_inicial), "Tentativas restantes: %d", MAX_TENTATIVAS);
     gtk_label_set_text(GTK_LABEL(labelTentativas), texto_inicial);
     gtk_box_pack_start(GTK_BOX(caixaVertical), labelTentativas, FALSE, FALSE, 5);
     
@@ -358,17 +321,15 @@ void iniciar_jogo_termo(int argc, char *argv[])
             gtk_widget_set_vexpand(label, TRUE);
             gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
             gtk_widget_set_valign(label, GTK_ALIGN_CENTER);
-            
             GtkStyleContext *context = gtk_widget_get_style_context(label);
             gtk_style_context_add_class(context, "grid-tile");
-
             gtk_grid_attach(GTK_GRID(grid), label, j, i, 1, 1);
             grid_tiles[i][j] = label;
         }
     }
 
     GtkWidget *entrada = gtk_entry_new();
-    gtk_entry_set_max_length(GTK_ENTRY(entrada), TAMANHO_PALAVRA);
+    gtk_entry_set_max_length(GTK_ENTRY(entrada), 0);
     gtk_entry_set_alignment(GTK_ENTRY(entrada), 0.5);
     gtk_box_pack_start(GTK_BOX(caixaVertical), entrada, FALSE, FALSE, 5);
 
